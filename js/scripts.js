ac2c9b53ce8186d3b7ae73d0ae352044cf431d07
abe <abeybarra@gmail.com>
2020-01-14 15:23:28 -0800

    be much simpler

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,6 @@
 $(document).ready(function() {
-  $(".JS-clickable").click(function() {
-    $("#JS-definition").fadeToggle();
-  });
-});
-
-$(document).ready(function() {
-  $(".Operator-clickable").click(function() {
-    $("#Operator-definition").fadeToggle();
-  });
-});
-
-$(document).ready(function() {
-  $(".Var-Clickable").click(function() {
-    $("#Var-Definition").fadeToggle();
-  });
-});
-$(document).ready(function() {
-  $(".Var-Name-Clickable").click(function() {
-    $("#Var-Name-Definition").fadeToggle();
+  $(".well").click(function() {
+    //$("h3", this).fadeToggle();
+    $("p", this).slideToggle();
   });
 });
